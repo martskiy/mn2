9f0aadb1f9db2816584698121fc9a745e88cbf3f
gneqstr <bb704805@gmail.com>
2024-04-08 17:25:17 +0300

    sdasdaeee

--- src/pages/Desktop.js
+++ src/pages/Desktop.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./Desktop.css"; // Импортируем стили
 
-const Desktop = () => {
+const HomePage = () => {
   return (
-    <div>
-      LOH ZAIDI S TELEFONA
+    <div className="container">
+      <div className="text">Use your mobile device</div>
+      <div className="image-container">
+        <img
+          src="IMG_5556.PNG"
+          alt="Placeholder"
+          className="image"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Desktop
+export default HomePage;
