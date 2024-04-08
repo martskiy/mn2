4303d565a0db6bb3382ce18ad8006bd8f6ba1f98
gneqstr <bb704805@gmail.com>
2024-04-08 22:23:48 +0300

    sdasdaeeew

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './Loader.css'; // Стили для Loader
+import UserContext from "../../context/UserContext";
 
 const Loader = ({ duration = 10000 }) => {
+  const { fiveHundred, hundred } = useContext(UserContext)
   const [progress, setProgress] = useState(100);
 
   useEffect(() => {
     const startTime = Date.now();
-    const endTime = startTime + duration;
+    const timers = fiveHundred ? 0.5 : 1
+    const endTime = startTime + duration * timers;
 
     const intervalId = setInterval(() => {
       const now = Date.now();
@@ -21,7 +24,7 @@ const Loader = ({ duration = 10000 }) => {
     }, 100);
 
     return () => clearInterval(intervalId);
-  }, [duration]);
+  }, [duration, fiveHundred, hundred]);
 
   return (
     <div className="loader-container">
